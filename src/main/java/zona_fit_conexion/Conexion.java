c9b11c74b189087f7682e2ac8b469e32da02a8db
Maximiliano Brullo <maximilianobrullo4@gmail.com>
2024-10-25 12:58:57 -0300

    add Conexion Class

---
+++ src/main/java/zona_fit_conexion/Conexion.java
@@ -0,0 +1,29 @@
+package zona_fit_conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    public static Connection getConecion(){
+        Connection connection = null;
+        String baseDatos = "zona_fit_db";
+        String url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        String usuario = "root";
+        String password = "admin";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, usuario, password);
+        }catch (Exception e){
+            System.out.print("Error al conectar a la DB" + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        if(conexion.getConecion() != null){
+            System.out.println("Conexion exitosa!!");
+        }else{
+            System.out.println("No se puede conectar a la DB");
+        }
+    }
+}
