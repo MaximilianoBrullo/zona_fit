652f885e805e84e71d747172bbea64fbc4696089
MaximilanoBrullo <maximilianobrullo4@gmail.com>
2024-11-16 22:28:45 -0300

    -

---
+++ src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,62 @@
+package zona_fit.presentacion;
+
+
+import zona_fit.datos.ClienteDAO;
+import zona_fit.datos.InterfaceClienteDAO;
+import zona_fit.dominio.Cliente;
+
+import java.util.Scanner;
+
+public class ZonaFitApp {
+    public static void main(String[] args) {
+        zonaFitApp();
+    }
+
+    public static void zonaFitApp() {
+        boolean salir = false;
+        Scanner consola = new Scanner(System.in);
+        InterfaceClienteDAO clienteDAO = new ClienteDAO();
+        while (!salir) {
+            try{
+                mostrarMenu(consola);
+                //salir = ejecutarOpciones(consola, clienteDAO);
+            }catch (Exception e) {
+                System.out.println("Error al ejecutar opciones: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mostrarMenu(Scanner consola) {
+        System.out.print("""
+                        ***Zona Fit***'
+                        1. Listar Clientes
+                        2. Agregar Cliente
+                        3. Modificar Cliente
+                        4. Eliminar Cliente
+                        5. Buscar Cliente
+                        6. Salir
+                        """);
+        return Integer.parseInt(consola.nextLine());
+    }
+
+    private static boolean ejecutarOpciones(Scanner consola, int opcion,
+                                            InterfaceClienteDAO clienteDAO){
+        boolean salir = false;
+        switch (opcion) {
+            case 1:
+                System.out.println("--- Listado de clientes ---");
+                for(Cliente cliente : clienteDAO.listarClientes()){
+                    System.out.println(cliente);
+                }
+                return salir;
+
+                case 2:
+                    System.out.println("--- Agregar Cliente ---");
+                    clienteDAO.agregarCliente(new Cliente(
+
+                    ))
+
+        }
+    }
+}
